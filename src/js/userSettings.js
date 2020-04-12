2d92b4f761b80146c44b86546f7d46996f974429
Maeva Wolff <maeva.wolff@hetic.net>
2020-04-12 21:24:31 +0200

    user page added

---
+++ src/js/userSettings.js
@@ -0,0 +1,20 @@
+const setting = document.querySelector(".user__setting");
+let width = setting.offsetWidth;
+const container = document.querySelector(".user__settings");
+const account = document.getElementById("account");
+const sub = document.getElementById("sub");
+const active = document.querySelector(".user .active");
+
+console.log(width);
+
+sub.addEventListener("click", () => {
+  container.style.transform = `translateX(${-width}px)`;
+  account.classList.remove("active");
+  sub.classList.add("active");
+});
+
+account.addEventListener("click", () => {
+  container.style.transform = `translateX(${width}px)`;
+  sub.classList.remove("active");
+  account.classList.add("active");
+});
